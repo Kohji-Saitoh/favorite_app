8212fa5abb196e664ee275d54bf97e02b16b1f8b
Kohji-Saitoh <koji.s102030@gmail.com>
2023-04-03 14:12:09 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'spots#index'
+  
+  resources :spots do               #こちらの２行を追加
+    resource :likes, only: [:create, :destroy] 
+ end
 end
